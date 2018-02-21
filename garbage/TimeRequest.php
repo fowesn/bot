f49c0d3fdf432def9e4268cca05bab1fff7d364d
Kurenchuk Sergey <kurenchuksergey@gmail.com>
2018-02-22 02:16:00 +0300

    Вынос Api в отдельный файл
    Signed-off-by: Kurenchuk Sergey <kurenchuksergey@gmail.com>

---
+++ garbage/TimeRequest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kurenchuksergey
+ * Date: 14.02.18
+ * Time: 19:16
+ */
+
+namespace project\request;
+
+
+class TimeRequest implements iRequest {
+	private $request_params;
+
+	public function __construct($user_id) {
+		$date = date("H:i:s");
+		$this->request_params = array(
+			'message' => "Сейчас {$date}",
+			'user_id' => $user_id,
+			'access_token' => COMMUNITY_TOKEN,
+			'v' => VERSION_VK_API
+		);
+	}
+
+	public function getResult() {
+		return $this->request_params;
+
+	}
+}
