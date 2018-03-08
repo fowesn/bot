dd584ebbfc9764c0251ee9fbddc8ac5c3602a48a
Kurenchuk Sergey <kurenchuksergey@gmail.com>
2018-03-08 19:29:48 +0300

    Работа с БД
    Signed-off-by: Kurenchuk Sergey <kurenchuksergey@gmail.com>

---
+++ database/Task.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kurenchuksergey
+ * Date: 08.03.18
+ * Time: 18:39
+ */
+
+namespace database;
+
+
+class Task {
+	private $mysql;
+
+	public
+	public static function getTaskById($id) {
+
+	}
+
+	public static function getTasksByCategory
+
+	public static function getTasksByNumber
+
+	public static function getSolution
+
+	public static function getCategories
+
+	public static function getAnswer
+
+	public static function getScore
+
+	public static function getSolved
+
+	public static function checkAnswer
+
+}
