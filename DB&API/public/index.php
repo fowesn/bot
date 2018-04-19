327ea211e301f3806184753b2edc63e5186ce265
daswunderwaffle <antosha.nestuloff@yandex.ru>
2018-04-19 22:28:28 +0300

    API resources

--- DB&API/public/index.php
+++ DB&API/public/index.php
@@ -7,5 +7,42 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App();
 
+
+$app->get('/api/v1/problems/problem', function (Request $request, Response $response) {
+
+});
+
+
+$app->get('/api/v1/solutions/solution', function (Request $request, Response $response) {
+
+});
+
+
+$app->get('/api/v1/answers/answer', function (Request $request, Response $response) {
+
+});
+
+
+$app->post('/api/v1/answers/answer', function (Request $request, Response $response) {
+
+});
+
+
+$app->get('/api/v1/resources/resource', function (Request $request, Response $response) {
+
+});
+
+
+$app->put('api/v1/resources/resource', function (Request $request, Response $response)
+{
+
+});
+
+
+$app->get('/api/v1/problems/problem_types/problem_type', function (Request $request, Response $response) {
+
+});
+
+
 $app->run();
 ?>
