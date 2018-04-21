8dd6c0f4ef547d223b4597fd47c7a008aa69525d
daswunderwaffle <antosha.nestuloff@yandex.ru>
2018-04-21 23:32:22 +0300

    assignedAnswer added, assingAnswer edited

--- DB&API/classes/db/dbAssignment.php
+++ DB&API/classes/db/dbAssignment.php
@@ -6,18 +6,26 @@
  * Time: 6:26
  */
 
-class dbAssignment extends dbConnection
+class dbAssignment
 {
+    // для прототипа работает :)
     public static function assignProblem ($user_id, $problem_id)
     {
-        //$user = dbMisc::getGlobalUserId($user_id)
         $conn = dbConnection::getConnection();
-        $stmt = $conn->prepare('INSERT INTO  assignment (problem_id, user_id, assignment_last_answer, assigned, correct_answer_provided) VALUES (?, ?, NULL, ?, FALSE )');
-        $stmt->execute(array($problem_id, $user_id, time()));
+        $stmt = $conn->prepare('INSERT INTO  assignment (problem_id, user_id, assignment_last_answer, assigned, correct_answer_provided) VALUES (?, ?, NULL, NOW(), FALSE )');
+        $stmt->execute(array($problem_id, $user_id));
+        return true;
     }
 
     public static function assignAnswer ($user_id, $problem_id, $answer)
     {
+        $conn = dbConnection::getConnection();
+        $stmt = $conn->prepare('SELECT assignment_id FROM assignment WHERE problem_id = ? AND user_id = ?');
+        $stmt->execute(array($problem_id, $user_id));
+        $assignment_id = $stmt->fetch()['assignment_id'];
 
+        $stmt = $conn->prepare('INSERT INTO answer (assignment_id, solution_answer, solution_provided) VALUES (?, ?, NOW())');
+        $stmt->execute(array($assignment_id, $answer));
+        return true;
     }
 }
