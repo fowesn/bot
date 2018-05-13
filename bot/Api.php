7a903a6904036e5e207ec61b0321bab1a9630a84
Kurenchuk Sergey <kurenchuksergey@gmail.com>
2018-05-13 16:40:45 +0300

    Рефакторинг

--- bot/Api.php
+++ bot/Api.php
@@ -46,92 +46,29 @@ class Api {
 
     /**
 	 *
-	 * @param $user_id получатель изображения
-     * @param $image_path путь до файла изображения
+	 * @param $user_id string получатель изображения
+	 * @param $image_path string путь до файла изображения
      * @throws \Exception
-	 * @return массив полей загруженного изображения на сервер вк
+	 * @return array массив полей загруженного изображения на сервер вк
      */
 
     static public function pictureAttachmentMessageSend($user_id, $image_path) {
-//
-
-        /** @var  $type формат изображения */
-        $type = exif_imagetype($image_path);
-        /** @var  $mimeType string mime тип изображения для запроса */
-        $mimeType = image_type_to_mime_type($type);
-
-
-		$file = curl_file_create($image_path, $mimeType, 'filename.jpg');
-//		$file = LoadFile::getImage($image_path);
-
-		/** @var  $request_params  Надо попробовать с create ....*/
+		/** @var  $request_params - array параметров к запросу .... */
         $request_params = array("peer_id" => $user_id);
         $request_params = self::setVersionAndToken($request_params);
-        $result = json_decode(file_get_contents('https://api.vk.com/method/photos.getMessagesUploadServer?' .
-            "peer_id=" . $request_params["peer_id"] . "&access_token=" .
-            $request_params["access_token"] . "&v=" . $request_params["v"]));
+		$result = json_decode(file_get_contents('https://api.vk.com/method/photos.getMessagesUploadServer?' . http_build_query($request_params)));
 
         /** В случае ошибки запроса */
         if(isset($result->error))
             throw new RequestError(__FILE__." : ".__LINE__." ".$result->error->error_msg . " " . 'https://api.vk.com/method/photos.getMessagesUploadServer?' .
                 http_build_query($request_params), $result->error->error_code);
 
+		/** @var  $server - сервер загрузки изображения */
+		$server = parse_url($result->response->upload_url);
+		/** @var $result - содержит ответ сервера на загрузку изображения */
+		$result = LoadFile::sendData($server, LoadFile::getImage($image_path));
 
-        /** @var  $server сервер загрузки изображения */
-
-        $server = $result->response->upload_url;
-
-        $server = parse_url($server);
-
-//        /////////////////////////////////////////////
-		///////////////////////////////////////////////////
-		///
-		///
-		///
-		///
-		///
-		///
-		// устанавливаем соединение с сервером
-        $time_start = time();
-		$fp = fsockopen("ssl://".$server["host"], 443, $errno, $errstr, 5);
-		if (!$fp)
-			throw new \Exception("проблемы с сокетом".$server["host"]);
-		$boundary = "9e99e84655473cf6";
-		$content = LoadFile::getImage($image_path);
-		fwrite($fp, 'POST '.$server["path"].'?'.$server["query"].' HTTP/1.1'."\r\n");
-		fwrite($fp, 'Host: '.$server["host"]." \r\n");
-		fwrite($fp, 'Content-Type: multipart/form-data; boundary='.$boundary."\r\n");
-		fwrite($fp, 'Content-Length: '.strlen($content)."\r\n\r\n");
-		fwrite($fp, $content);
-		$result = '';
-
-			while ( !feof($fp) ) $result .= fgets($fp, 1024);
-			// закрываем соединение
-
-		fclose($fp);
-        $time_end = time();
-        throw new \Exception($time_end - $time_start);
-
-///		/** @var  $postParam поле с бинарным изображениям для POST запроса */
-//        $postParam = LoadFile::getImage($image_path);
-////		$postParam = array("photo"=>$file);
-//        //Отправляем файл на сервер
-////		$curlLog = fopen("curl.log","w");
-//        $ch = curl_init($server);
-//        curl_setopt($ch, CURLOPT_POST, true);
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//        curl_setopt($ch, CURLOPT_POSTFIELDS,$postParam);
-//        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data; boundary=------------------------9e99e84655473cf6'));
-////		curl_setopt($ch, CURLOPT_VERBOSE,true);
-////		curl_setopt($ch, CURLOPT_STDERR,$curlLog);
-////		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data-alternate'));
-//        /** @var  $photo_server_json Ответ на загрузку изображения */
-//
-//        $photo_server_json = json_decode(curl_exec($ch));
-//        curl_close($ch);
-
-//		fclose($curlLog);
-
+		/** temp */
 		$result = preg_split('/\n/',$result);
 
 		$photo_server_json = json_decode($result[count($result)-1]);
@@ -141,7 +78,7 @@ class Api {
         if(isset($photo_server_json->error))
             throw new RequestError(__FILE__." : ".__LINE__.$photo_server_json->error,$photo_server_json->error->error_code);
 
-        /** @var  $photo_save параметры для сохранения фото на сервере */
+		/** @var  $photo_save array параметры для сохранения фото на сервере */
         $photo_save = array(
             "server" => $photo_server_json->server,
             "hash" => $photo_server_json->hash,
@@ -149,15 +86,66 @@ class Api {
             );
         $photo_save = self::setVersionAndToken($photo_save);
 
-        /** @var Получение id и других параметров изображения на сервере VK $result */
+		/** @var array $result Получение id и других параметров изображения на сервере VK */
         $result = json_decode(file_get_contents('https://api.vk.com/method/photos.saveMessagesPhoto?' . http_build_query($photo_save)));
         /** В случае если произошла ошибка */
         if (isset($result->error))
            throw new RequestError(__FILE__." : ".__LINE__.$result->error->error_msg,$result->error->error_code);
 
-        $result = $result->response[0];
+		/** @var array $result */
+		if (isset($result->response[0])) {
+			$result = $result->response[0];
+		}
+
         return $result;
     }
+
+	/**
+	 * @param $user_id string -  id пользователя
+	 * @param $document_path string - url к документу
+	 * @return array[]|false|mixed|string|string[] - возвращает информацию о документе
+	 * @throws RequestError в случае ошибок при запросе к вк API
+	 * @throws \Exception - в случае отсутсвие файла
+	 */
+	static public function documentAttachmentMessageSend($user_id, $document_path) {
+
+		/** @var  $request_params - array параметров к запросу .... */
+		$request_params = array("peer_id" => $user_id, "type" => "doc");
+		$request_params = self::setVersionAndToken($request_params);
+		$result = json_decode(file_get_contents('https://api.vk.com/method/docs.getMessagesUploadServer?' . http_build_query($request_params)));
+		/** проверка на успешность */
+		if (isset($result->error))
+			throw new RequestError(__FILE__ . " : " . __LINE__ . " " . $result->error->error_msg . " " . 'https://api.vk.com/method/docs.getMessagesUploadServer?' .
+				http_build_query($request_params), $result->error->error_code);
+		/** @var  $server - сервер загрузки изображения */
+		$server = parse_url($result->response->upload_url);
+
+		/** @var $result - содержит ответ сервера на загрузку изображения */
+		$result = LoadFile::sendData($server, LoadFile::getDocument($document_path));
+
+		/** temp */
+		$result = preg_split('/\n/', $result);
+		$document_server_json = json_decode($result[count($result) - 1]);
+		if (isset($document_server_json->error))
+			throw new RequestError(__FILE__ . " : " . __LINE__ . $document_server_json->error, $document_server_json->error->error_code);
+		/** @var array $document_save - параметры для сохранения документа */
+		$document_save = array(
+			"file" => $document_server_json->server,
+			"title" => "Задание",
+			"tags" => "персональный бот"
+		);
+		$document_save = self::setVersionAndToken($document_save);
+		$result = json_decode(file_get_contents('https://api.vk.com/method/docs.save?' . http_build_query($document_save)));
+		if (isset($result->error))
+			throw new RequestError(__FILE__ . " : " . __LINE__ . $result->error->error_msg, $result->error->error_code);
+
+		if (isset($result->response[0])) {
+			$result = $result->response[0];
+		}
+		return $result;
+
+
+	}
 	/**
 	 *  Метод является оберткой метода getHistory, в коем не участвует
 	 * параметр start_message_id
@@ -248,7 +236,7 @@ class Api {
  */
 
 class RequestError extends \Exception {
-	public function __construct($message, $code = 0, Exception $previous = null) {
+	public function __construct($message, $code = 0, \Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
 }
--- bot/LoadFile.php
+++ bot/LoadFile.php
@@ -10,36 +10,97 @@ namespace api;
 
 
 class LoadFile {
-	static public function getImage($url){
+	/**
+	 * @param $photo_path string путь к фотографий
+	 * @return array
+	 * @throws \Exception
+	 */
+	static public function getImage($photo_path) {
 		ini_set("allow_url_fopen",true);
-		/** @var  $type формат изображения */
-		$type = exif_imagetype($url);
+		/** @var  $type string - формат изображения */
+		$type = exif_imagetype($photo_path);
 		if($type == false)
-			throw new Exception(__FILE__." : ".__LINE__." File not found ".$url);
+			throw new \Exception(__FILE__ . " : " . __LINE__ . " File not found " . $photo_path);
 		/** @var  $mimeType string mime тип изображения для запроса */
 		$mimeType = image_type_to_mime_type($type);
+
 		ob_start();
 		ob_clean();
-		echo "--9e99e84655473cf6\r\n";
-		echo "Content-Disposition: form-data; name=\"photo\"; filename=\"filename.png\"\r\n";
-		echo "Content-Type: ".$mimeType."\r\n";
-		echo 'Content-Transfer-Encoding: binary'."\r\n\r\n";
-//		header('Content-Type: '.$mimeType);
-//		header('Content-Disposition: attachment; filename=' . basename($url));
-//		header('Content-Length: ' . filesize($url));
-
-//		readfile($url);
-		if ($fd = fopen($url, 'rb')) {
+		if ($fd = fopen($photo_path, 'rb')) {
 			while (!feof($fd)) {
 				print fread($fd, 1024);
 			}
 			fclose($fd);
 		}
-		echo "\r\n--9e99e84655473cf6--";
 		$out = ob_get_contents();
 		ob_end_clean();
-		return $out;
+		/** @var array $result */
+		$result = array("mime" => $mimeType,
+			"fieldName" => "photo",
+			"fileName" => basename($photo_path),
+			"content" => $out);
+		return $result;
+	}
 
+
+	/**
+	 * @param $document_path - url к документу
+	 * @return array - документ
+	 * @throws \Exception - в случае отсутвие файла
+	 */
+	public static function getDocument($document_path) {
+		ini_set("allow_url_fopen", true);
+		if (!fopen($document_path, "r"))
+			throw new \Exception(__FILE__ . " : " . __LINE__ . "File not found " . $document_path);
+		$mimeType = mime_content_type($document_path);
+		ob_start();
+		ob_clean();
+		if ($fd = fopen($document_path, 'rb')) {
+			while (!feof($fd)) {
+				print fread($fd, 1024);
+			}
+			fclose($fd);
+		}
+		$out = ob_get_contents();
+		ob_end_clean();
+		$result = array("mime" => $mimeType,
+			"fieldName" => "file",
+			"fileName" => basename($document_path),
+			"content" => $out);
+		return $result;
+
+	}
+
+	/**
+	 * @param $server array
+	 * @param $data array
+	 * @throws \Exception
+	 */
+	public static function sendData($server, $data) {
+		/** https */
+		$fp = fsockopen("ssl://" . $server["host"], 443, $errno, $errstr, 5);
+		if (!$fp)
+			throw new RequestError(__FILE__ . ":" . __LINE__ . "проблемы с сокетом" . $server["host"]);
+		/** @var string разделитель полей на сокете $boundary */
+		$boundary = md5(uniqid(time()));
+		/** подготовка контента */
+		$content = "--" . $boundary . '\r\n';
+		$content .= 'Content-Disposition: form-data; name=\"' . $data['fieldName'] . '\"; filename=\"' . $data['fileName'] . '\"\r\n';
+		$content .= "Content-Type: " . $data['mime'] . "\r\n";
+		$content .= 'Content-Transfer-Encoding: binary' . "\r\n\r\n";
+		$content .= $data['content'] . '\r\n';
+		$content .= "--" . $boundary . '--';
+
+		fwrite($fp, 'POST ' . $server["path"] . '?' . $server["query"] . ' HTTP/1.1' . "\r\n");
+		fwrite($fp, 'Host: ' . $server["host"] . " \r\n");
+		fwrite($fp, 'Content-Type: multipart/form-data; boundary=' . $boundary . "\r\n");
+		fwrite($fp, 'Content-Length: ' . strlen($content) . "\r\n\r\n");
+		fwrite($fp, $content);
+		$result = '';
+		while (!feof($fp)) $result .= fgets($fp, 1024);
+		// закрываем соединение
+		fclose($fp);
+		return $result;
 	}
 
 }
