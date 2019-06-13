a2b6bd92241276970eb46fcb07abb1f77815756f
fowesn <fowesn@bk.ru>
2019-06-13 22:49:25 +0300

    связь разбор

--- MainModule/handler/message/Answer.php
+++ MainModule/handler/message/Answer.php
@@ -49,52 +49,46 @@ class Answer
 	public static function getAnalysis($userID, $taskID) {
         if(!isset($userID))
             throw new \Exception(__FILE__ . " : " . __LINE__ . " Не указан user_id");
-		$task = (int)$userID ^ (int)$taskID;
-		$params = array("problem_id" => $task, "user_id" => $userID, "service" => "vk");
-		$request_params = http_build_query($params);
 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, HOST_API . '/problems/solution?' . $request_params);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-		$result = curl_exec($ch);
-		$result = json_decode($result);
-		//проверка кодов http
-		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		if ($code != 200) {
-			$message = $code . ". " . self::$server_error_message;
-			return array("user_id" => $userID, "message" => $message);
-		}
+        $url = HOST_API . '/problems/' . $taskID . '/solution?' . http_build_query(array("user" => $userID, "service" => 'vk'));
 
-		//ошибки пользователя
-		if ($result->success !== "true") {
-			$message = $result->error->message;
-		} else {
-			// если ошибок нет, то собирается сбщ с разбором
-			$message = "";
-			for ($i = 0; $i < count($result->data); $i++)
-				switch ($result->data[$i]->type) {
-					case 'pdf-файл':
-						// тут нужен attachment документа
-						$attachment = \MainModule\VKAPI::documentAttachmentMessageSend($userID,$result->data[$i]->content,
-							"разбор " . $taskID, "бот по информатике");
-						break;
-					case 'изображение':
-						// attachment изображения
-						$attachment = \MainModule\VKAPI::pictureAttachmentMessageSend($userID,$result->data[$i]->content);
-						break;
-					case 'ссылка':
-						$message = $result->data[$i]->content;
-						break;
-					case 'текст':
-						if(preg_match("#^http#i", $result->data[$i]->content))
-							$attachment = \MainModule\VKAPI::pictureAttachmentMessageSend($userID, $result->data[$i]->content);
-						else
-							$message .= "\r\n" . $result->data[$i]->content;
-						break;
-					default:
-						break;
-				}
-		}
+        $code = substr(get_headers($url)[0], 9, 3);
+
+        if($code == 200)
+        {
+            $result = json_decode(file_get_contents($url));
+            if($result->status === 'a1-0')
+                $message = "Задание с таким номером я тебе не выдавал. Чтобы посмотреть список номеров нерешённых тобой заданий, напиши мне \"задания\"";
+            else
+                $message = "Разбор задания " . $taskID;
+            for ($i = 0; $i < count($result->data->resources); $i++)
+                switch ($result->data->resources[$i]->type) {
+                    case 'pdf':
+                        // тут нужен attachment документа
+                        $attachment = \MainModule\VKAPI::documentAttachmentMessageSend($userID,$result->data->resources[$i]->content,
+                            "разбор " . $taskID, "бот по информатике");
+                        break;
+                    case 'изображение':
+                        // attachment изображения
+                        $attachment = \MainModule\VKAPI::pictureAttachmentMessageSend($userID,$result->data->resources[$i]->content);
+                        break;
+                    case 'ссылка':
+                        $message = $result->data->resources[$i]->content;
+                        break;
+                    case 'текст':
+                        if(preg_match("#^http#i", $result->data->resources[$i]->content))
+                            $attachment = \MainModule\VKAPI::pictureAttachmentMessageSend($userID, $result->data->resources[$i]->content);
+                        else
+                            $message .= "\r\n" . $result->data->resources[$i]->content;
+                        break;
+                    default:
+                        break;
+                }
+        }
+        else
+            $message = $code . ". " . self::$server_error_message . "\r\n\r\n";
+
+
 		if(isset($attachment))
             return array("user_id" => $userID, "message" => $message, "attachment" => $attachment);
 		else
