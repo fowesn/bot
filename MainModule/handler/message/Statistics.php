f6049f718a0e4a3a2ea3bb6a3d2fae3705211e00
fowesn <fowesn@bk.ru>
2019-06-14 21:56:19 +0300

    связь статистика

--- MainModule/handler/message/Statistics.php
+++ MainModule/handler/message/Statistics.php
@@ -9,9 +9,25 @@
 namespace MainModule\handler\message;
 class Statistics
 {
+    private static $server_error_message = "Что-то пошло не так. Попробуй снова!";
     public static function getTasksStatistics($userID)
     {
-        $message = $userID;
+
+        $url = HOST_API . '/assignments?' . http_build_query(array("user" => $userID, "service" => 'vk'));
+
+        $code = substr(get_headers($url)[0], 9, 3);
+
+        if($code == 200)
+        {
+            $result = json_decode(file_get_contents($url));
+            $message = "Количество запрошенных заданий: " . $result->data->overall . "\r\n
+                        Количество решенных заданий: " . $result->data->solved . "\r\n
+                        Количество заданий, на которые был запрошен правильный ответ: " . $result->data->answer_requested . "\r\n
+                        Количество нерешённых заданий: " . $result->data->unsolved;
+
+        }
+        else
+            $message = $code . ". " . self::$server_error_message;
         return array("user_id" => $userID, "message" => $message);
     }
 
